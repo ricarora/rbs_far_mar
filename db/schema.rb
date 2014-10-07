acf9939e51fde8cc625b4bb95ce319d287d3f741
Brenda Praggastis <bpraggastis@gmail.com>
2014-10-07 16:31:50 -0700

    added vendors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007231841) do
+ActiveRecord::Schema.define(version: 20141007233038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20141007231841) do
     t.string   "county"
     t.string   "state"
     t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "vendors", force: true do |t|
+    t.string   "name"
+    t.integer  "no_of_employees"
+    t.integer  "market_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
