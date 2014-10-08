b1d4f2efb3bad5688482eeec7ef692f09c6e2039
Brenda Praggastis <bpraggastis@gmail.com>
2014-10-08 14:40:15 -0700

    modified markets_controller and required name for vendor

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -5,6 +5,12 @@ class MarketsController < ApplicationController
   end
 
   def create
+    @market = Market.new(params.require(:market).permit(:name,:address,:city,:county,:state,:zip))
+    if @market.save
+      redirect_to root_path
+    else
+      render :new  #this renders new.html.erb again
+    end
   end
 
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,8 @@
 class Vendor < ActiveRecord::Base
+  has_many :products
+  has_many :sales
+
+  belongs_to :market
+
+  validates :name, presence: true
 end
