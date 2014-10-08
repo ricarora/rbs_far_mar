cbadaddfdb2aae546402ade0272cba54ab102d99
Brenda Praggastis <bpraggastis@gmail.com>
2014-10-08 14:24:46 -0700

    made create method

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -5,12 +5,6 @@ class MarketsController < ApplicationController
   end
 
   def create
-    @market = Market.new(params.require(:banana).permit(:title,:content))
-    if @market.save
-      redirect_to root_path
-    else
-      render :new  #this renders new.html.erb again
-    end
   end
 
 end
