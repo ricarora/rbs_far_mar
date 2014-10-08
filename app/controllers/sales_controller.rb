f47f7bd1480543a7a8b6e92366c039e8eb7bd748
Richa <ricarora@gmail.com>
2014-10-08 14:26:35 -0700

    add index, new, create in sales controller

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,19 @@
 class SalesController < ApplicationController
+
+  def index
+    @sales = Sale.all
+  end
+
+  def new
+    @sale = Sale.new
+  end
+
   def create
+    @sale = Sale.new(params.require(:sale).permit(:amount, :purchase_time, :purchase_date, :vendor_id, :product_id))
+    if @sale.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
