d5ad0d20d191662128d983adf08140e84c1bdfe3
Brenda Praggastis <bpraggastis@gmail.com>
2014-10-08 14:25:08 -0700

    made create method for vendors and forms

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,6 +1,18 @@
 class VendorsController < ApplicationController
+
+  def new
+    @vendor = Vendor.new
+  end
+
   def create
+    @vendor = Vendor.new(params.require(:vendor).permit(:name,:no_of_employees,:market_id))
+    if @vendor.save
+      redirect_to root_path
+    else
+      render :new  #this renders new.html.erb again
+    end
 
   end
+
 
 end
