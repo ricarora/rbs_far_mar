0c26d7cbfa07069eed7426da76fbeb33b62c6fbb
Richa <ricarora@gmail.com>
2014-10-09 16:45:01 -0700

    added product functionality - add, edit, delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,13 +11,27 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(params.require(:product).permit(:name, :vendor_id))
     if @product.save
-      redirect_to root_path
+      redirect_to product_new_path
     else
       render :new
     end
   end
 
   def delete
-    @product = nil
+    @product = Product.find(params[:id]).destroy
+    redirect_to product_new_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(params.require(:product).permit(:name, :vendor_id))
+      redirect_to product_new_path
+    else
+      render :edit
+    end
   end
 end
