a14b38a26c5050638da3e36eb3b350c4eb8d240a
Brenda Praggastis <bpraggastis@gmail.com>
2014-10-10 23:01:46 -0700

    Created Seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+(1..10).each do
+  market = {
+    name: Faker::Address.city + " Farmer's Market",
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: "WA",
+    county: "King",
+    zip: Faker::Address.zip
+  }
+
+  Market.create(market)
+end
+
+(1..100).each do
+
+  name = Faker::Company.name
+  username = /[a-z]+/.match(name.downcase)[0]
+  vendor = {
+  name: name,
+  no_of_employees: rand(1..10),
+  market_id: rand(1..3),
+  username: username,
+  password: username.reverse}
+
+  Vendor.create(vendor)
+end
+
+(1..1000).each do
+
+  name = Faker::Commerce.product_name
+  product = {
+    name: name,
+    vendor_id: rand(1..100
+
+    )
+  }
+
+  Product.create(product)
+
+end
